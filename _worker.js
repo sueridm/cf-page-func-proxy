a551487314efebd290b093f7ec742f2d0b903c2c
sueridm <76246973+sueridm@users.noreply.github.com>
2023-08-19 01:57:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="cdn.rayks.com";
+        url.hostname="cdn1.rayks.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
